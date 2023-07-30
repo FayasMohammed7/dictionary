578ff23a9a597eb1cd02081b133cfc3e6fbe15e3
Fayas Mohammed N <124593766+FayasMohammed7@users.noreply.github.com>
2023-07-30 16:28:53 +0530

    Dictionary APP
    Dictionary website used to know the meaning and pronunciation of any word

---
+++ script.js
@@ -0,0 +1,48 @@
+const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").
+    value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+        <div class="word">
+            <h3>${inpWord}</h3>
+            <button onclick="playSound()">
+                
+                <i class="fas fa-meteor" style="color: black;"></i>
+                
+            </button>
+        </div>
+        <div class="details">
+            <p>${data[0].meanings[0].partOfSpeech}</p>
+            <p>${data[0].phonetics[1].text}</p>
+        </div>
+
+        <div>
+            <audio controls src="${data[0].phonetics[0].audio}"></audio>
+        </div>
+
+        <p class="word-meaning marg ">
+            ${data[0].meanings[0].definitions[0].definition}
+        </p>
+        <p class="word-example">
+            ${data[0].meanings[0].definitions[0].example || ""}
+        </p>`;
+        
+        sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+function playSound(){
+    sound.play();
+}
+
